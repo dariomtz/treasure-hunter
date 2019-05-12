ea3ad90018866aa47cabc7e0e9488c54597be3a9
Dario Martinez <34457517+dariomtz@users.noreply.github.com>
2019-05-11 20:21:25 -0500

    game_functions.h
    Now, the function that gets the shape of the pieces does it by getting a csv that is in resources
    -300 lines of code yei

--- game_functions.h
+++ game_functions.h
@@ -449,427 +449,55 @@ MAP initialize_Map1(MAP map) {
     return map;
 }
 
-void initializeShapesRect(SDL_Rect arrayRects[]){
-    for (int i = 0; i < 240; i++) {
-        //x
-        switch (i) {
-            case 147:
-                arrayRects[i].x =(int) ((3/(float)16)*SCALE);
-                break;
-            case 24:
-            case 31:
-            case 34:
-            case 40:
-            case 81:
-            case 83:
-            case 86:
-            case 96:
-            case 106:
-            case 136:
-            case 146:
-            case 176:
-            case 186:
-                arrayRects[i].x = SCALE/4;
-                break;
-            case 56:
-            case 66:
-            case 76:
-            case 101:
-            case 102:
-            case 116:
-            case 121:
-            case 122:
-            case 126:
-            case 141:
-            case 142:
-            case 156:
-            case 161:
-            case 162:
-            case 166:
-            case 181:
-            case 182:
-            case 201:
-            case 202:
-            case 221:
-            case 222:
-                arrayRects[i].x = (int) ((6/(float)16)*SCALE);
-                break;
-            case 33:
-            case 42:
-            case 71:
-            case 72:
-            case 73:
-            case 82:
-            case 124:
-            case 125:
-            case 144:
-            case 145:
-            case 164:
-            case 165:
-            case 184:
-            case 185:
-            case 204:
-            case 205:
-            case 224:
-            case 225:
-                arrayRects[i].x = (int) ((7/(float)16)*SCALE);
-                break;
-            case 104:
-            case 105:
-                arrayRects[i].x = SCALE/2;
-                break;
-            default:
-                arrayRects[i].x = 0;
-                break;
-        }
-        //y
-        switch (i) {
-            case 24:
-                arrayRects[i].y = SCALE/4;
-                break;
-            case 147:
-                arrayRects[i].y = (int) ((6/(float)16)*SCALE);
-                break;
-            case 5:
-            case 15:
-            case 25:
-            case 35:
-            case 45:
-            case 55:
-            case 56:
-            case 65:
-            case 66:
-            case 76:
-            case 86:
-            case 96:
-            case 106:
-            case 116:
-            case 126:
-            case 136:
-            case 146:
-            case 156:
-            case 166:
-            case 176:
-            case 186:
-                arrayRects[i].y = (int) ((7/(float)16)*SCALE);
-                break;
-            case 87:
-            case 97:
-            case 107:
-            case 117:
-            case 127:
-            case 137:
-                arrayRects[i].y = SCALE/2;
-                break;
-            case 104:
-            case 114:
-            case 124:
-            case 134:
-            case 144:
-            case 154:
-            case 164:
-            case 174:
-            case 184:
-            case 194:
-            case 204:
-            case 214:
-            case 224:
-            case 234:
-                arrayRects[i].y = (int) ((11/(float)16)*SCALE);
-                break;
-                
-            default:
-                arrayRects[i].y = 0;
-                break;
-        }
-        
-        //w
-        switch (i) {
-            case 56:
-            case 66:
-            case 76:
-            case 116:
-            case 126:
-            case 156:
-            case 166:
-                arrayRects[i].w = 25;
-                break;
-            case 24:
-            case 86:
-            case 96:
-            case 104:
-            case 105:
-            case 106:
-            case 114:
-            case 115:
-            case 124:
-            case 125:
-            case 134:
-            case 135:
-            case 136:
-            case 144:
-            case 145:
-            case 146:
-            case 154:
-            case 155:
-            case 164:
-            case 165:
-            case 174:
-            case 175:
-            case 176:
-            case 184:
-            case 185:
-            case 186:
-            case 194:
-            case 195:
-            case 204:
-            case 205:
-            case 214:
-            case 215:
-            case 224:
-            case 225:
-            case 234:
-            case 235:
-                arrayRects[i].w = 50;
-                break;
-            case 32:
-            case 33:
-            case 42:
-            case 43:
-            case 51:
-            case 52:
-            case 53:
-            case 64:
-            case 71:
-            case 72:
-            case 73:
-            case 74:
-            case 82:
-            case 92:
-                arrayRects[i].w = 56;
-                break;
-            case 101:
-            case 102:
-            case 111:
-            case 112:
-            case 121:
-            case 122:
-            case 131:
-            case 132:
-            case 141:
-            case 142:
-            case 147:
-            case 151:
-            case 152:
-            case 161:
-            case 162:
-            case 171:
-            case 172:
-            case 181:
-            case 182:
-            case 191:
-            case 192:
-            case 201:
-            case 202:
-            case 211:
-            case 212:
-            case 221:
-            case 222:
-            case 231:
-            case 232:
-                arrayRects[i].w = 62;
-                break;
-            case 30:
-            case 31:
-            case 34:
-            case 40:
-            case 41:
-            case 44:
-            case 81:
-            case 83:
-            case 91:
-            case 93:
-                arrayRects[i].w = 75;
-                break;
-            case 5:
-            case 15:
-            case 25:
-            case 35:
-            case 45:
-            case 55:
-            case 65:
-            case 87:
-            case 97:
-            case 107:
-            case 117:
-            case 127:
-            case 137:
-                arrayRects[i].w = 87;
-                break;
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-            case 10:
-            case 11:
-            case 12:
-            case 13:
-            case 14:
-            case 16:
-            case 17:
-            case 18:
-            case 19:
-            case 20:
-            case 21:
-            case 22:
-            case 26:
-            case 27:
-            case 28:
-            case 29:
-            case 36:
-            case 37:
-            case 38:
-            case 39:
-            case 46:
-            case 47:
-            case 48:
-            case 49:
-            case 50:
-            case 54:
-            case 57:
-            case 58:
-            case 59:
-            case 60:
-            case 61:
-            case 67:
-            case 68:
-            case 69:
-            case 70:
-            case 77:
-            case 78:
-            case 79:
-            case 84:
-            case 85:
-            case 88:
-            case 89:
-            case 94:
-            case 95:
-            case 98:
-            case 99:
-            case 108:
-            case 109:
-            case 118:
-            case 119:
-            case 128:
-            case 129:
-            case 138:
-            case 139:
-            case 148:
-            case 149:
-                arrayRects[i].w = 100;
-                break;
-                
-            default:
-                arrayRects[i].w = 0;
-                break;
-        }
-        
-        //h
-        switch (i) {
-            case 56:
-            case 66:
-            case 76:
-            case 116:
-            case 126:
-            case 156:
-            case 166:
-                arrayRects[i].h = 25;
-                break;
-            case 104:
-            case 114:
-            case 124:
-            case 134:
-            case 144:
-            case 154:
-            case 164:
-            case 174:
-            case 184:
-            case 194:
-            case 204:
-            case 214:
-            case 224:
-            case 234:
-                arrayRects[i].h = 31;
-                break;
-            case 24:
-            case 86:
-            case 96:
-            case 106:
-            case 136:
-            case 146:
-            case 176:
-            case 186:
-            case 5:
-            case 15:
-            case 25:
-            case 35:
-            case 45:
-            case 55:
-            case 65:
-            case 87:
-            case 97:
-            case 107:
-            case 117:
-            case 127:
-            case 137:
-                arrayRects[i].h = 50;
-                break;
-            case 102:
-            case 112:
-            case 122:
-            case 132:
-            case 142:
-            case 152:
-            case 162:
-            case 172:
-            case 182:
-            case 192:
-            case 202:
-            case 212:
-            case 222:
-            case 232:
-                arrayRects[i].h = 56;
-                break;
-            case 147:
-                arrayRects[i].h = 62;
-                break;
-            case 105:
-            case 115:
-            case 125:
-            case 135:
-            case 145:
-            case 155:
-            case 165:
-            case 175:
-            case 185:
-            case 195:
-            case 205:
-            case 215:
-            case 225:
-            case 235:
-                arrayRects[i].h = 68;
-                break;
-                
-            default:
-                arrayRects[i].h = 100;
-                break;
-        }
-    }
+void initializeShapesRect(SDL_Rect arrayRects[], char dir[]){
+	int count = 0;
+	while (dir[count] != '\0') {
+		count ++;
+	}
+	int i=count;
+	while (dir[i] != '/') {
+		dir[i] = '\0';
+		i--;
+	}
+	printf("%s\n", dir);
+	
+	char filePath[255];
+	strcpy(filePath, dir);
+	strcat(filePath, "resources/game_hitbox.csv");
+	FILE * file = fopen(filePath, "r");
+	char line[256];
+
+	if(file != NULL){
+		while (fgets(line, 256, file)){
+			int piece[5];
+			char num[5];
+			i = 0;
+			int j = 0;
+			int k = 0;
+			while(line[i] != '\n'){
+				if (line[i] == ',' ) {
+					j = - 1;
+				}else{
+					if ((j==0 && i != 0) || line[i+1] == '\n') {
+						piece[k] = atoi(num);
+						k++;
+					}
+					num[j] = line[i];
+					num[j+1] = '\0';
+				}
+				j++;
+				i++;
+			}
+			arrayRects[piece[0]].x = (int) (piece[1] / (float) 16 * SCALE);
+			arrayRects[piece[0]].y = (int) (piece[2] / (float) 16 * SCALE);
+			arrayRects[piece[0]].w = (int) (piece[3] / (float) 16 * SCALE);
+			arrayRects[piece[0]].h = (int) (piece[4] / (float) 16 * SCALE);
+		}
+		
+	}else{
+		printf("ERROR: Unable to read CSV File \"resources/game_hitbox.csv\"");
+	}
+	fclose(file);
 }
 
 #endif /* game_functions_h */
